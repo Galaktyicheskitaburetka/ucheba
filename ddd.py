a9a073c2398d4440d1361122adaa0ce7cdbf5747
Galakyicheskitaburetka <prostobaran47@gmail.com>
2024-11-07 09:59:05 +0700

    add file for studies

---
+++ ddd.py
@@ -0,0 +1,14 @@
+#import math
+#p = math.pi
+#r = int(input('введите радиус'))
+#def ArCi(p,r):
+#    return p*r**2
+#print(ArCi(p,r))
+import math
+C = 12.01115
+O = 15.9994
+moll_mass = C + O*2
+# def rounding(C,O):
+#     return math.ceil(moll_mass)
+# print(rounding(C,O))
+
