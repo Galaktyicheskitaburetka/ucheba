78e3d702247146a9c2c5d10d8bd69dc046c73e91
Galakyicheskitaburetka <prostobaran47@gmail.com>
2024-12-12 10:01:29 +0700

    Add files via upload

---
+++ control work.py
@@ -0,0 +1,23 @@
+
+
+
+#number 1
+#n2
+
+number = int(input('ведите число от 1 до 10'))
+flag = False
+while not flag:
+    if number <= 0 or number > 10:
+        print('от 1 до 10')
+        number = int(input('введите число от 1 до 10'))
+    else :
+        flag = True
+for i in range (1,11):
+    print(number * i)
+
+import math
+n = int(input())
+k = int(input())
+def C(n,k) :
+    print(math.factorial(n) / (math.factorial(n) * math.factorial(n-k)))
+
