ba628fdeeb22ef7d7bf669c2b2b22dc4b4ee47ef
Galakyicheskitaburetka <prostobaran47@gmail.com>
2025-01-23 09:59:27 +0700

    Add files via upload

---
+++ гойда.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+# Чтение Excel-файла
+df_excel = pd.read_excel('ZZZ.xlsx')
+
+# Посмотреть первые 10 строк
+print(df_excel.head(4))
+
+print(df_excel.sample(4))
+
+# Посмотреть информацию о таблице
+print(df_excel.info())
+
+
+print("GOYDAA")
+print(df_excel.iloc[3])
